b7bacf3d2dd96cba8215f614d877c6459269031a
Victor Ajagunna <victor-ajagunna@hotmail.com>
2022-10-23 12:27:26 +0200

    loader first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
+import { render } from '@testing-library/react';
+import React, { useState } from 'react';
 import './App.css';
 import Loader from './components/loader/Loader';
 
-function App() {
+export default function App () {
+  const [users, setUsers] = useState([]);
+  const [ isLoading, setIsLoading ] = useState(false);
+  const [ errorMessage, setErrorMessage ] = useState("");
+  const handleFetch = () => {
+    setIsLoading(true);
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => {
+        setUsers(data);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setErrorMessage("Oops! Unable to fetch users!");
+        setIsLoading(false);
+      });
+  };
+  const renderUser = (
+    <div className="userlist-container">
+      { users.map((item, index) => (
+        // <img src={ item.avatar } alt="" />
+        <div className="userDetails">
+          <div className="userName">{ item.name }</div>
+          <div className="userEmail">{ item.email }</div>
+          <div className="userAddresses">{ item.addresses }</div>
+          <div className="userPhone">{ item.phone }</div>
+        </div>
+      )) }
+    </div>
+  );
   return (
     <div className="App">
-      <Loader/>
+      { isLoading ? <Loader /> : renderUser }
+      { errorMessage && <div className="error">{ errorMessage }</div> }
+      <button onClick={ handleFetch } disabled={ isLoading }>Fetch Users </button>
     </div>
   );
 }
 
-export default App;
+// export default App;
